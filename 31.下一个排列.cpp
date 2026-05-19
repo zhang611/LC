67d6452df086c1c332ffba2b17673bb1895b573d
zzc <johndoe@example.com>
2026-05-19 11:20:58 +0800

    .

--- 31.下一个排列.cpp
+++ 31.下一个排列.cpp
@@ -8,6 +8,40 @@
 // @lc code=start
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {}
+    void nextPermutation(vector<int>& nums) {
+        int k = nums.size() - 1;
+        while (k > 0 && nums[k] <= nums[k - 1]) k--;
+        if (k == 0)
+            reverse(nums.begin(), nums.end());
+        else {
+            int t = k;
+            while (t < nums.size() && nums[t] > nums[k - 1]) t++;
+            swap(nums[k - 1], nums[t - 1]);
+            reverse(nums.begin() + k, nums.end());
+        }
+    }
 };
 // @lc code=end
+
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         int k = nums.size()-1;
+//         while(k > 0 && nums[k] <= nums[k-1]) k--;
+//         if (k == 0) reverse(nums.begin(), nums.end());
+//         else {
+//             int t = k;
+//             while(t < nums.size() && nums[t] > nums[k-1]) t++;
+//             swap(nums[k-1], nums[t-1]);
+//             reverse(nums.begin() + k, nums.end());
+//         }
+//     }
+// };
+
+// 1 2 3 4 5
+// 1 3 2 5 4
+
+// k 3
+// t 4
+// 1 3 4 5 2
+// 1 3 4 2 5
